48f7ed68fc6104a93c6b592d824976c120dd9f8a
Abhijeet Bhatt <im0125@IMEDIA.LOCAL>
2020-03-08 15:42:29 +0530

    first commit'

---
+++ pages/login.js
@@ -0,0 +1,28 @@
+let login = function(){
+
+    let email= element(By.id('email'));
+    let password =element(By.id('password'));
+    let loginbtn= element(By.id('btnSubmit'));
+
+    this.get = function(url){
+        browser.driver
+        browser.ignoreSynchronization = true;
+        browser.manage().timeouts().implicitlyWait(3000)
+        browser.driver.manage().window().maximize();
+        console.log("For login driver is executed");
+        browser.sleep(20000);
+        browser.get(url);
+    }
+    this.enterEmail =  function(emailid){
+        email.sendKeys(emailid);
+    };
+
+    this.enterPassword = function(pass){
+        password.sendKeys(pass);
+    }
+
+    this.loginbtn =  function(){
+        loginbtn.click();
+    }
+}
+module.exports = new login();
