0cd0632f0228e5568555719ab21d6e0d87ab1e83
Abhijeet Bhatt <im0125@IMEDIA.LOCAL>
2020-03-16 12:00:54 +0530

    Added allur reports

--- pages/boardOperations.js
+++ pages/boardOperations.js
@@ -4,16 +4,31 @@ let boardOperations = function(){
    // let shapes = element(By.css('#shapes-menu-option'));
     let shapes = element(By.xpath('//li[@id="shapes-menu-option"]'));
     let rectangle= element(By.xpath('//a[@class="shape-rect"]'));
-
+    let addComment = element(By.xpath("//li[@class='box-height']"));
+    let addCommtopic = element(By.xpath("//input[@id='showCommentTopic']"));
+    let addNewcomment= element(By.xpath("//div[@id='newComment']"));
+    let postComment = element(By.css("[ng-click='createGroupInitializeComment()']"));
+    let asdf = element()
     this.clickBoard= async()=>{
        await selectBoard.click();
     }
-
     this.selectShape=async()=>{
         await  shapes.click();
     }
    this.selectRectangle=async()=>{
         await rectangle.click();
+    }
+    this.addCommentbox = async()=>{
+        await addComment.click();
+    }
+    this.addTopic = async(dynamicTopic)=>{
+        await addCommtopic.sendKeys(dynamicTopic);
+    }
+    this.addNewcomm = async(dynamicComment) => {
+        await addNewcomment.sendKeys(dynamicComment);
+    }
+    this.clickPost = async()=>{
+        await postComment.click();
     }
 }
 module.exports = new boardOperations();
